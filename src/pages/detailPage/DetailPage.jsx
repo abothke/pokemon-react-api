a382f18755c7e3225006395042e86ca90485ba5b
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-31 21:11:33 +0100

    detail page, bg color changes by types name, css updates

--- src/pages/detailPage/DetailPage.jsx
+++ src/pages/detailPage/DetailPage.jsx
@@ -3,6 +3,7 @@ import { mainContext } from "../../assets/context/mainProvider";
 import { useState } from "react";
 import "./detailPage.css"
 import { useParams } from "react-router-dom";
+import Heder from "../../components/header/Header";
 
 
 const DetailPage = () => {
@@ -11,48 +12,49 @@ const DetailPage = () => {
     setPokemonId(id)
     console.log(pokemon);
     const [hidden, setHidden] = useState(true);
-    // const {pokomenId} = useParams() hier werder die params geholt aus der url
-    // setPokemonId(pokomenId) hier wird die id gesetzt
 
-
-
-
+    //falls id ist kleiner als 10 commt eine 0 vorne und acuh bei 100
+    const idMitNullen = `#${id.padStart(3, '0')}`
 
     return (<>
+        <Heder />
         {
 
             pokemon ? (
+                // <main >
                 <div className="detailContainer">
-
                     <div className="imageBgd"><img src={pokemon?.sprites?.other?.showdown?.front_default} alt={pokemon.name} /></div>
-                    <h1>#{pokemon?.id} {pokemon?.name}</h1>
+                    <h1 className="title">{idMitNullen} {pokemon?.name}</h1>
 
-                    {/* hier mache ich die powers aber bin noch nicht fertig */}
                     <div className="powers">
                         {pokemon?.types?.map((powers, index) => (
-                            <button key={index}>{powers.type.name}</button>
+                            <p className={`color-${powers.type.name}`} key={index}>{powers.type.name}</p>
                         ))}
                     </div>
                     <div className="attacks">
-                            <h2 onClick={() => {
+                        <h2 onClick={() => {
                             setHidden(!hidden)
-                            }}>Attacks and Movements</h2>
-                            { hidden === false ? (
-                                                           <ul>
-                                                           {pokemon?.moves?.map((move, index) => (
-                                                           <li className="attack" key={index}>{move.move.name}</li>
-                                                           ))}
-                                                           </ul> 
-                            ) : (
-                                null
-                            ) }
+                        }}>Attacks and Movements</h2>
+                        {hidden === false ? (
+                            <ul>
+                                {pokemon?.moves?.map((move, index) => (
+                                    <li className="attack" key={index}>{move.move.name}</li>
+                                ))}
+                            </ul>
+                        ) : (
+                            null
+                        )}
 
                     </div>
                 </div>
+                // </main>
             )
                 :
                 (
-                    <p>...loading</p>
+                    < div className="detailContainer">
+                        <h4>... loading the pokomen <details></details></h4>
+                    </div>
+
                 )
 
         }
