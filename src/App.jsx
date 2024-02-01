a76dcc05a18712b31688467e715779f80409fed7
abothke <andrebothke@gmail.com>
2024-02-01 14:18:08 +0100

    Burgermenu und "Back" auf Detailpage done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import './App.css'
 import DetailPage from './pages/detailPage/DetailPage'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
+import PokemonTypeMenu from './components/pokemonTypeMenu/pokemonTypeMenu'
 
 function App() {
   return (
     <>
-    
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/pokemon/:id' element={<DetailPage />} />
