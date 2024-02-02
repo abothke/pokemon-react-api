50569123de3cd3828118aa8d451873e9e06d3361
abothke <andrebothke@gmail.com>
2024-02-02 09:12:24 +0100

    fixed showing all pokemons after being at detailpage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from '../../assets/images/International_Pokémon_logo.png'
 import SearchBar from '../searchBar/SearchBar'
 import { Link, useParams } from 'react-router-dom'
 import "./header.css"
+import { mainContext } from '../../assets/context/mainProvider'
 
 const Header = () => {
-
+  const { setPokemons, backupPokemons } = useContext(mainContext)
   const { id } = useParams()
   return (
     <>
       <header>
-              <Link to="/"><img src={logo} alt="logo" /></Link>
+              <Link to="/" onClick={() => setPokemons(backupPokemons)}><img src={logo} alt="logo" /></Link>
               {id ? null : <SearchBar />}
               {id ?
-              <Link to="/">Back</Link>
+              <Link to="/" onClick={() => setPokemons(backupPokemons)}>Back</Link>
               : null }      
               </header>
     </>
